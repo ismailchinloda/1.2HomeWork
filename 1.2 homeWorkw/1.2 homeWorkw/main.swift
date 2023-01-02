36c6616b9e3534ed9fcb81cfc36f67513dd5f4f8
ismailchinloda <113357847+ismailchinloda@users.noreply.github.com>
2023-01-02 19:41:32 +0600

    Add files via upload

---
+++ 1.2 homeWorkw/1.2 homeWorkw/main.swift
@@ -0,0 +1,43 @@
+//
+//  main.swift
+//  1.2 homeWorkw
+//
+//  Created by Ismail Chinloda on 2/1/23.
+//
+
+import Foundation
+//edef
+//№1. Используя функцию составить следующую программу:
+
+//Нахождение квадрата числа
+//func square(numbersquare: Int) -> Int {
+//    return numbersquare * numbersquare
+//}
+//print(square(numbersquare: 6))
+
+//Нахождение куба числа
+//func cube(numbercube: Int) -> Int {
+//    return numbercube * numbercube * numbercube
+//}
+//print(cube(numbercube: 5))
+
+//Нахождение периметра куда
+//func perimetr(a: Int,b: Int,c: Int,d:Int){
+//    print(a + b + c + d)
+//}
+//perimetr(a: 10, b: 5, c: 10, d: 5)
+
+//Нахождение площади прямоугольника
+//func ploshad(a: Int,b: Int){
+//    print("площадь прямоугольника равна \(a * b)")
+//}
+//ploshad(a: 40, b: 25)
+
+//Нахождение площади круга
+func areaOfCircle(radius: Double)->Double{
+    return 3.14*(radius*radius)
+}
+print(areaOfCircle(radius: 3))
+
+//№2 Создать 4 функции для калькулятора ( + - / *) с двумя операндами и произвести 10 разных вычислений с разными числами
+
